b3cb755a74eb8af60926ff93904509b864382e84
Peter Major <38843393+majorpiotr@users.noreply.github.com>
2018-11-18 22:37:26 +0100

    3D effects

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ $( document ).ready(function() {
    			{
 				// if it is it returns its ID, to $(window).scroll()
 				return $(elem).attr("id");
+
    			}
    			else
    			{
@@ -21,7 +22,7 @@ $( document ).ready(function() {
    		else
    		{
    			// if element has no id (likely null or undefinded) it returns FALSE and stops function
-   		
+   			//alert($(elem).attr("id"));
    			return false;
    		}
    		
@@ -35,13 +36,16 @@ $( document ).ready(function() {
    			// if element is on screen and has own ID it starts animations (by adding css property to this)
    			// Animation mentioned above is in style.css file in css directory 
    			//  apear_from_nowhere in /css/style.css
-    		$("#"+id_card).css("animation-name", "apear_from_nowhere" );
+   			//alert(id_card);
+    		$("#"+id_card).css("animation-name", "wake_up" );
 			$("#"+id_card).css("animation-duration", "3s");
-			$("#"+id_card).css("animation-timing-function", "linear");
-			$("#"+id_card).css("opacity", "1");
+			$("#"+id_card).css("animation-timing-function", "ease-in");
+			$("#"+id_card).addClass("opacity-100");	
 			// it removes class "show_animation" in order to avoid errors, and machining next element.
 			// without this function will machining the same element over and over .
-			$("#"+id_card).removeClass("show_animation");			
+			$("#"+id_card).removeClass("show_animation");		
+			$("#"+id_card).addClass("show_done");		
+					
    		}
    		
 	})
@@ -89,12 +93,175 @@ $( document ).ready(function() {
 			// 	mouse leave- button are white
 			$(this).removeClass("bg-warning");
 		});
+	$(".set_background").click(function() 
+		{
+		$(this).toggleClass("opacity-0");
+		}
+	);
+
+// for changing languages
+	$(".language").click(function()
+		{
+		$(".nav-link:contains('Discover Us')").text("Tref uns");
+		}
+	);
+	$( function() {
+    	$( "#calendar_legend" ).accordion({
+      		collapsible: true,
+      		active:1
+    	});
+  	} );
+  	$( function() {
+    	$( ".accordion" ).accordion({
+      		collapsible: true,
+      		active:1
+    	});
+  	} );
+  	function un_rotated(elem)
+  	{
+  		if ($(elem).hasClass("rotated-10"))
+  		{  		
+  			$(elem).removeClass("rotated-10",500, "easeOutBounce");
+		}
+  	}
+  	function make_small(elem)
+  	{
+  		
+			$(elem).addClass("smallable",500, "easeOutBounce");
+		
+  	}
+  	function make_small_most_left(elem)
+  	{
+  			$(elem).addClass("smallable_3d_most_left",500, "easeOutBounce");
+  	}
+  	function make_small_just_left(elem)
+  	{
+  			$(elem).addClass("smallable_3d_left",500, "easeOutBounce");
+  	}
+  	function make_small_just_right(elem)
+  	{
+			$(elem).addClass("smallable_3d_right",500, "easeOutBounce");
+  	}
+  	function make_small_most_right(elem)
+  	{
+			$(elem).addClass("smallable_3d_most_right",500, "easeOutBounce");
+  	}
+
+  	function un_make_small(elem)
+  	{
+  		if ($(elem).hasClass("rotated-10"))
+  		{  		
+  		$(elem).addClass("rotated-10",500, "easeOutBounce");	
+  		}
+  		$(elem).removeClass("smallable",600, "easeOutBounce");
+  		
+  	}
+  	function un_make_small_most_left(elem)
+  	{
+  			$(elem).removeClass("smallable_3d_most_left",500, "easeOutBounce");
+  	}
+  	function un_make_small_just_left(elem)
+  	{
+  			$(elem).removeClass("smallable_3d_left",500, "easeOutBounce");
+  	}
+  	function un_make_small_just_right(elem)
+  	{
+			$(elem).removeClass("smallable_3d_right",500, "easeOutBounce");
+  	}
+  	function un_make_small_most_right(elem)
+  	{
+			$(elem).removeClass("smallable_3d_most_right",500, "easeOutBounce");
+  	}
+  	
+  	$(".small_siblings").hover(
+		function(){
+			if ( $(this).hasClass("show_done") ) 
+			{
+				un_rotated($(this).siblings("rotated-10"));
+				make_small($(this).siblings("no_3d"));
+				make_small_most_left($(this).siblings(".most_left"));
+				make_small_just_left($(this).siblings(".just_left"));
+				make_small_just_right($(this).siblings(".just_right"));
+				make_small_most_right($(this).siblings(".most_right"));
+			
+			}
+
+			if(!$(this).hasClass("show_animation"))
+			{
+				un_rotated($(this).siblings("rotated-10"));
+				make_small($(this).siblings(".no_3d"));
+				make_small_most_left($(this).siblings(".most_left"));
+				make_small_just_left($(this).siblings(".just_left"));
+				make_small_just_right($(this).siblings(".just_right"));
+				make_small_most_right($(this).siblings(".most_right"));
+				
+			}
+
+		},
+		function(){
+			if ( $(this).hasClass("show_done") ) 
+			{
+				un_make_small($(this).siblings(".no_3d"));
+				un_make_small_most_left($(this).siblings(".most_left"));
+				un_make_small_just_left($(this).siblings(".just_left"));
+				un_make_small_just_right($(this).siblings(".just_right"));
+				un_make_small_most_right($(this).siblings(".most_right"));
+
+			}
+
+			if(!$(this).hasClass("show_animation"))
+			{
+				un_make_small($(this).siblings(".no_3d"));
+				un_make_small_most_left($(this).siblings(".most_left"));
+				un_make_small_just_left($(this).siblings(".just_left"));
+				un_make_small_just_right($(this).siblings(".just_right"));
+				un_make_small_most_right($(this).siblings(".most_right"));
+
+			}
+		});
+  	
+  	function image_box()
+  	{
+  		//$("#h_test").addClass("lay_down"); /* Standard syntax */
+  		
+  	}
+
+  	function history_play()
+  	{
+
+  		year = $( "#slider" ).slider("value");
+  		year ++;
+  		$( "#slider" ).slider("value" , year);
+  		if (year>$( "#slider" ).slider("option", "max")) 
+  		{
+  			year=$( "#slider" ).slider("option", "min");
+  			$( "#slider" ).slider("value" , year);
+  		}
+  		$( "#custom-handle" ).text(year);
+  		image_box();
+
+  	}
+  	$("#history_play").click(function(){
+  		
+  		var myVar = window.setInterval(history_play, 10000);
+  	});
 
 
-// test
-	 
-	// alert($('.carousel').carousel());
-// 
+  	 $( function() {
+    var handle = $( "#custom-handle" );
+    $( "#slider" ).slider({
+    range: "max",
+    min: 1965,
+    max: 2018,
+    value: 1965,
+      create: function() {
+        handle.text( $( this ).slider( "value" ) );
+      },
+      slide: function( event, ui ) {
+        handle.text( ui.value );
+      }
+    });
+  } );
 
 	// 	About fuction $(".navbar").hover() showed  bellow :
 	//	This code hides navbar competely in order to make easier to watch photos, and read webpage. In one word site looks more beautiful.
